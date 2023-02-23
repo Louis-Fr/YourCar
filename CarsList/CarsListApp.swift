9b7d1edd675df9d61b725768449196c99bbbfca0
Louis Rey <louisreypro@yahoo.com>
2023-02-23 16:07:09 +0100

    Initial Commit

---
+++ CarsList/CarsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsListApp.swift
+//  CarsList
+//
+//  Created by Louis REY on 23/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CarsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
