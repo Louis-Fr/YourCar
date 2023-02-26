05eab18ff8bc70d1de139111cb2623b5ace2051a
Louis Rey <louisreypro@yahoo.com>
2023-02-26 11:51:53 +0100

    first commit

---
+++ CarsList/AddCarView.swift
@@ -0,0 +1,20 @@
+//
+//  AddCarView.swift
+//  CarsList
+//
+//  Created by Louis REY on 25/02/2023.
+//
+
+import SwiftUI
+
+struct AddCarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddCarView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddCarView()
+    }
+}
---
+++ CarsList/DataController.swift
@@ -0,0 +1,8 @@
+//
+//  DataController.swift
+//  CarsList
+//
+//  Created by Louis REY on 23/02/2023.
+//
+
+import Foundation
